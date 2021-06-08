96ff91f844fdcc801328f4bcfbee80658eaa129a
Artem Marhaza <artemmrgz@gmail.com>
2021-06-08 18:30:36 +0200

    refactoring build_diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,23 +3,35 @@ from gendiff.constants import ADDED, CHANGED, REMOVED, NESTED, UNCHANGED
 
 
 def build_diff(old, new):
-    keys = set(old) | set(new)
     difference = {}
-    for key in sorted(keys):
+    removed_keys = old.keys() - new.keys()
+    difference.update(
+        {
+            key: (REMOVED, old.get(key))
+            for key in sorted(removed_keys)
+        }
+    )
+    added_keys = new.keys() - old.keys()
+    difference.update(
+        {
+            key: (ADDED, new.get(key))
+            for key in sorted(added_keys)
+        }
+    )
+    for key in sorted(old.keys() & new.keys()):
         old_value = old.get(key)
         new_value = new.get(key)
-        children = isinstance(old_value, dict) and isinstance(new_value, dict)
-        if children:
+        has_children = (
+            isinstance(old_value, dict)
+        ) and (
+            isinstance(new_value, dict)
+        )
+        if has_children:
             difference[key] = (NESTED, build_diff(old_value, new_value))
-        elif key in old and key in new:
-            if old_value == new_value:
-                difference[key] = (UNCHANGED, new_value)
-            else:
-                difference[key] = (CHANGED, old_value, new_value)
-        elif key in old and key not in new:
-            difference[key] = (REMOVED, old_value)
+        elif old_value == new_value:
+            difference[key] = (UNCHANGED, new_value)
         else:
-            difference[key] = (ADDED, new_value)
+            difference[key] = (CHANGED, old_value, new_value)
     return difference
 
 
